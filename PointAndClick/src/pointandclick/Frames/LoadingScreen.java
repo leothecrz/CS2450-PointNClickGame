fd3bf013205cb3c963cf490c22b1da771c4bed7a
Vivian Trieu <vdtrieu@cpp.edu>
2022-09-16 03:34:46 -0700

    Basic LoadingScreen UI

--- PointAndClick/src/pointandclick/Frames/LoadingScreen.java
+++ PointAndClick/src/pointandclick/Frames/LoadingScreen.java
@@ -4,19 +4,31 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class LoadingScreen extends JPanel {
-    private JLabel titleLabel;
-    private Timer timer;
 
+public class LoadingScreen extends JPanel {
+    // private final JLabel titleLabel;
     public LoadingScreen(ActionListener listener) {
         setLayout(new FlowLayout());
-        titleLabel = new JLabel("<html><div style='text-align: center;'>Point and Click Game<br />By: Team Goofy Goobers</div></html>");
-        add(titleLabel);
-
+        //titleLabel = new JLabel("<html><div style='text-align: center;'>Point and Click Game<br />By: Team Goofy Goobers</div></html>");
+        //add(titleLabel);
+        
         // Set up timer
-        timer = new Timer(3000, listener);
+
+        Timer timer = new Timer(3000, listener);
         timer.setInitialDelay(3000); // Trigger timer after 3 seconds
         timer.start();
     }
-
+    @Override
+    public void paintComponent(Graphics g){
+                super.paintComponent(g);
+                Font titleFont = new Font("Courier New", Font.BOLD, 25);
+                Font teamFont = new Font("Courier New", Font.PLAIN, 15);
+                Graphics2D g2 = (Graphics2D) g;
+                g2.setFont(titleFont);
+                g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                g2.drawString("Point and Click Game", 150, 80);
+                g2.setFont(teamFont);
+                g2.drawString("Team Goofy Goobers", 210, 300);
+   
+    }
 }
