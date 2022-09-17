6d1be5e5460c521c4c110b09d7db1d8cbaca2d2b
jasroop-singh <113493289+jasroop-singh@users.noreply.github.com>
2022-09-16 18:14:27 -0700

    Update Credits.java

--- PointAndClick/src/pointandclick/Frames/Credits.java
+++ PointAndClick/src/pointandclick/Frames/Credits.java
@@ -13,4 +13,19 @@ public class Credits extends JPanel {
         add(new JLabel("Credits"));
         add(backButton);
     }
+    
+    @Override
+    public void paintComponent(Graphics g){
+                super.paintComponent(g);
+                Font personFont = new Font("Courier New", Font.PLAIN, 20);
+                
+                Graphics2D g2 = (Graphics2D) g;
+                g2.setFont(personFont);
+                g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                g2.drawString("Leonardo Davalos, #", 150, 120);
+                g2.drawString("Matthew Finerty, #", 150, 180);
+                g2.drawString("Jasroop Singh, #", 150, 240);
+                g2.drawString("Noris Tang, #", 150, 300);
+                g2.drawString("Vivian Trieu, #", 150, 360);
+    }
 }
