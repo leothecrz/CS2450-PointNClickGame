384777fddfec3a03eb04f9d5b334d12880847995
jasroop-singh <113493289+jasroop-singh@users.noreply.github.com>
2022-09-18 18:12:41 -0700

    Update Credits.java

--- PointAndClick/src/pointandclick/Frames/Credits.java
+++ PointAndClick/src/pointandclick/Frames/Credits.java
@@ -9,23 +9,23 @@ public class Credits extends JPanel {
         setLayout(new FlowLayout());
         JButton backButton = new JButton("Back");
         backButton.addActionListener(listener);
-
-        add(new JLabel("Credits"));
         add(backButton);
     }
     
-    @Override
+   @Override
     public void paintComponent(Graphics g){
                 super.paintComponent(g);
-                Font personFont = new Font("Marker Felt", Font.PLAIN, 20);
-                
+                Font personFont = new Font("Marker Felt", Font.PLAIN, 18);
+                Font creditsFont = new Font("Marker Felt", Font.BOLD, 35);
                 Graphics2D g2 = (Graphics2D) g;
-                g2.setFont(personFont);
+                g2.setFont(creditsFont);
                 g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-                g2.drawString("Leonardo Davalos, #", 165, 90);
-                g2.drawString("Matthew Finerty, #", 165, 150);
-                g2.drawString("Jasroop Singh, #", 165, 210);
-                g2.drawString("Noris Tang, #", 165, 270);
-                g2.drawString("Vivian Trieu, #", 165, 330);
+                g2.drawString("Credits", 245,70);
+                g2.setFont(personFont);
+                g2.drawString("Leonardo Davalos, #", 165, 100);
+                g2.drawString("Matthew Finerty, #", 165, 160);
+                g2.drawString("Jasroop Singh, #", 165, 220);
+                g2.drawString("Noris Tang, #", 165, 280);
+                g2.drawString("Vivian Trieu, #", 165, 340);
     }
 }
