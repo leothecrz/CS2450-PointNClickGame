4230c3bfbc59318b2fd1189b876075b08a0ae38f
triph <1747088+entriphy@users.noreply.github.com>
2022-09-19 16:15:37 -0700

    Hangman: show found letters on screen + code cleanup

--- PointAndClick/src/pointandclick/Frames/hangmanResources/HangmanGamePanel.java
+++ PointAndClick/src/pointandclick/Frames/hangmanResources/HangmanGamePanel.java
@@ -10,35 +10,26 @@ import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
-import java.awt.event.ActionEvent;
 
-
-/**
- *  
- *
- */
-public class HangmanGamePanel extends JPanel{
-    
+public class HangmanGamePanel extends JPanel {
+    // Constants
+    private static final int MAX_ERRORS = 6;
     private static final int ALPHABET_COUNT = 26;
     private static final String KEYS = "abcdefghijklmnopqrstuvwxyz";
     private static final String[] WORDBANK = {
         "abstract", "cemetery", "nurse", "pharmacy", "climbing"
     };
-    
-    public static final int MAX_ERRORS = 6;
-    
-    private JPanel topPanel;
-    private JPanel bottomPanel;
-    
-    private JLabel wordSpaces;
-    
+
+    // Components
+    private JPanel topPanel; // Hangman panel
+    private JPanel bottomPanel; // Keyboard panel
     private JButton skipButton;
-    private JButton[] keyButtons;
-        
+    private JButton[] keyButtons; // Keyboard buttons
+    
+    // Model
     private String wordToFind; // The current word
     private char[] wordFound; // Char array of found characters in the word
     private int errors; // Number of incorrect letters guessed
-    
     private int playerScore;
     
     /**
@@ -55,49 +46,35 @@ public class HangmanGamePanel extends JPanel{
          * Handles events that may occur from button presses such as a Game Loss or a Game Victory.
          */
         ActionListener buttonsGameListener = evt -> { //Button Listener
-                
             char pressedLetter = evt.getActionCommand().charAt(0);
-            int keynum = pressedLetter - 97; 
-            //System.out.println(keyButtons[keynum].getText());
-            keyButtons[keynum].setEnabled(false); // Disable each letter after it has been used.
+            keyButtons[KEYS.indexOf(pressedLetter)].setEnabled(false); // Disable each letter after it has been used.
             
             int index = wordToFind.indexOf(pressedLetter);
-            if( index != -1){
-              
-                do{
+            if (index != -1){
+                do {
                     wordFound[index] = pressedLetter;
                     wordToFind = wordToFind.replaceFirst(String.valueOf(pressedLetter), "_");             
                     index = wordToFind.indexOf(pressedLetter);
-                
-                }while(index != -1);
-                
-                System.out.println(wordFoundContent());
+                } while(index != -1);
+                repaint();
                 
                 boolean isWordFound = true;
-                for(int i=0; i<wordFound.length; i++){
-                    if(wordFound[i] == '_')
-                            isWordFound = false;
+                for (int i = 0; i < wordFound.length; i++){
+                    if (wordFound[i] == '_') {
+                        isWordFound = false;
+                        break;
+                    }
                 }
                 
-                if(isWordFound){
-                    
-                    Timer gameWinTimer = new Timer(0, skipAndEndListener); // A Timer with a 0 delay calls on skipListener
-                    gameWinTimer.setRepeats(false);
-                    gameWinTimer.setActionCommand("Word Found");
-                    gameWinTimer.start();
-                    
+                if (isWordFound) {
+                    skipAndEndListener.actionPerformed(evt);
                 }
                 
             } else {
                 System.out.println("Not in Word");
-                if(errors == MAX_ERRORS-1){
-                    
-                    Timer gameLossTimer = new Timer(0, skipAndEndListener); // A Timer with a 0 delay calls on skipListener
-                    gameLossTimer.setRepeats(false);
-                    gameLossTimer.setActionCommand("Word Not Found");
-                    gameLossTimer.start();
-                    
-                } else{
+                if (errors == MAX_ERRORS - 1){
+                    skipAndEndListener.actionPerformed(evt);
+                } else {
                     errors++;
                 }
                 System.out.println("Errors: " + errors);
@@ -107,22 +84,13 @@ public class HangmanGamePanel extends JPanel{
                
         };
         
-        /**
-         * 
-         */
-        ActionListener ScreenUpdatesLister = evt -> { // Update Screen For Game start
-            
-        };
-        
-        // TOP PANEL
-        
+        // Top panel
         topPanel = new JPanel();
-        
         topPanel.setLayout(null);
         topPanel.setPreferredSize(new Dimension(600, (int)(400*0.55))); // HARD CODED PANEL SIZES
         topPanel.setBackground(Color.GRAY); // USED TO DIFFIRENTIATE
         topPanel.setOpaque(false);
-        
+
         skipButton = new JButton("Skip");
         skipButton.addActionListener(skipAndEndListener);
         skipButton.setActionCommand("Skip");
@@ -130,86 +98,72 @@ public class HangmanGamePanel extends JPanel{
         
         topPanel.add(skipButton);
         
-        add(topPanel);
-        
-        // BOTTOM PANEL
+        // Bottom panel
         bottomPanel = new JPanel();
+        bottomPanel.setLayout(new FlowLayout());
+        // bottomPanel.setLayout(new GridLayout(4, 26)); // Alternate layout: GRID LAYOUT 2x13
         bottomPanel.setPreferredSize(new Dimension(595, (int)(400*0.4))); // HARD CODED PANEL SIZES
         bottomPanel.setBackground(Color.DARK_GRAY); // USED TO DIFFIRENTIATE
-        //bottomPanel.setLayout(new GridLayout(4, 26)); // GRID LAYOUT 2x13
-        bottomPanel.setLayout(new FlowLayout());
+
+        // Add panels
+        add(topPanel);
         add(bottomPanel);
         
+        
+        // Keyboard buttons
         keyButtons = new JButton[ALPHABET_COUNT];
         Font buttonFont = new Font(null, Font.PLAIN, 15);
-        for(int i=0; i<ALPHABET_COUNT; i++){
+        for (int i = 0; i < ALPHABET_COUNT; i++){
             keyButtons[i] = new JButton(); 
-            //keyButtons[i].setSize(40, 25);
             keyButtons[i].setText(String.valueOf(KEYS.charAt(i))); // Each Key Represents its letter
             keyButtons[i].addActionListener(buttonsGameListener); // AllButtons Connected To Same Listener
             keyButtons[i].setActionCommand(String.valueOf(KEYS.charAt(i))); // Buttons Have Action Command Correspondint To Its Letter. Key a has command "a".
-            
             keyButtons[i].setFont(buttonFont);
             keyButtons[i].setEnabled(true);
             
             bottomPanel.add(keyButtons[i]); // Add keyButtons to bottomPanel's grid. 
         }
     }
-    
-    /**
-     * 
-     */
+
     public void resetButtons(){
-        for(int i=0; i<ALPHABET_COUNT; i++){  
-            keyButtons[i].setEnabled(true);
-        }
+        for (JButton keyButton : keyButtons)
+            keyButton.setEnabled(true);
     }
-    
-    /**
-     * 
-     */
+
     public void startGame(){
        resetButtons();
        playerScore = 100;
        errors = 0;
-       int r = (int) (Math.random() * (4)); 
-       wordToFind = WORDBANK[r];                // Choose random word
+       wordToFind = WORDBANK[(int) (Math.random() * WORDBANK.length)]; // Choose random word from wordbank
        wordFound = new char[wordToFind.length()];
-       
        for (int i = 0; i < wordFound.length; i++) {
-               wordFound[i] = '_';
+            wordFound[i] = '_';
        }
-       
-       
-       //JLabel wordSpaces = new JLabel (wordFoundContent()); // Doesn't work but an attempt to display empty letters
-       //topPanel.add(wordSpaces);                                     // like this "_ _ _ _"
     }
     
-    // This makes a stand for hanging the man but it appears behind the gray JPanel... i dont know how to bring it in front of the gray background
     @Override
     public void paintComponent(Graphics g){
-                super.paintComponent(g);
-                
-                Graphics2D g2 = (Graphics2D) g;
-                
-                g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-                g2.fillRect(100, 20, 5, 200);
-                g2.fillRect(30, 220, 180, 5);
-                g2.fillRect(100, 20, 80, 5);
-                g2.fillRect(180, 20, 5, 30);
-                
-    } 
+        super.paintComponent(g);
+        
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.fillRect(100, 20, 5, 200);
+        g2.fillRect(30, 220, 180, 5);
+        g2.fillRect(100, 20, 80, 5);
+        g2.fillRect(180, 20, 5, 30);
+        g2.setFont(new Font("Marker Felt", Font.BOLD, 35));
+        g2.drawString(wordFoundContent(), 300, 220);
+    }
     
     private String wordFoundContent() {
         StringBuilder b = new StringBuilder();
         for (int i = 0; i < wordFound.length; i++) {
             b.append(wordFound[i]);
 
-            if (i <wordFound.length - 1) {
+            if (i < wordFound.length - 1) {
                 b.append(" ");
             }
         }         
         return b.toString();
     }
-    
 }
