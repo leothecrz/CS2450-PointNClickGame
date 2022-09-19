7b7fe507d6b69ec4bbff0146e5cd05885c54fb04
Leonardo Davalos <32350741+leothecrz@users.noreply.github.com>
2022-09-19 14:37:31 -0700

    Game Win and Game Loss activate actionListener in Hangman.java

--- PointAndClick/src/pointandclick/Frames/hangmanResources/HangmanGamePanel.java
+++ PointAndClick/src/pointandclick/Frames/hangmanResources/HangmanGamePanel.java
@@ -10,6 +10,7 @@ import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
 
 
 /**
@@ -37,58 +38,78 @@ public class HangmanGamePanel extends JPanel{
     private String wordToFind; // The current word
     private char[] wordFound; // Char array of found characters in the word
     private int errors; // Number of incorrect letters guessed
-    private int guesses; // Number of all letters guessed
+    
+    private int playerScore;
     
     /**
      * 
-     * @param skipListener 
+     * @param skipAndEndListener 
      */
-    public HangmanGamePanel(ActionListener skipListener){
+    public HangmanGamePanel(ActionListener skipAndEndListener){
         super();
         setPreferredSize(new Dimension(600, 400));
         setBackground(Color.lightGray);
         
         /**
-         * 
+         * Action Listener. Handles all interaction with the keyboard on the bottom panel.
+         * Handles events that may occur from button presses such as a Game Loss or a Game Victory.
          */
         ActionListener buttonsGameListener = evt -> { //Button Listener
                 
             char pressedLetter = evt.getActionCommand().charAt(0);
             int keynum = pressedLetter - 97; 
-
-            System.out.println(keyButtons[keynum].getText());
-            
+            //System.out.println(keyButtons[keynum].getText());
             keyButtons[keynum].setEnabled(false); // Disable each letter after it has been used.
             
             int index = wordToFind.indexOf(pressedLetter);
             if( index != -1){
-                
+              
                 do{
                     wordFound[index] = pressedLetter;
-                    //wordToFind = wordToFind.replace(pressedLetter, '_');
-                    wordToFind = wordToFind.replaceFirst(String.valueOf(pressedLetter), "_");
-                    
+                    wordToFind = wordToFind.replaceFirst(String.valueOf(pressedLetter), "_");             
                     index = wordToFind.indexOf(pressedLetter);
                 
                 }while(index != -1);
                 
                 System.out.println(wordFoundContent());
                 
+                boolean isWordFound = true;
+                for(int i=0; i<wordFound.length; i++){
+                    if(wordFound[i] == '_')
+                            isWordFound = false;
+                }
+                
+                if(isWordFound){
+                    
+                    Timer gameWinTimer = new Timer(0, skipAndEndListener); // A Timer with a 0 delay calls on skipListener
+                    gameWinTimer.setRepeats(false);
+                    gameWinTimer.setActionCommand("Word Found");
+                    gameWinTimer.start();
+                    
+                }
+                
             } else {
                 System.out.println("Not in Word");
-                if(errors == MAX_ERRORS){
+                if(errors == MAX_ERRORS-1){
+                    
+                    Timer gameLossTimer = new Timer(0, skipAndEndListener); // A Timer with a 0 delay calls on skipListener
+                    gameLossTimer.setRepeats(false);
+                    gameLossTimer.setActionCommand("Word Not Found");
+                    gameLossTimer.start();
                     
                 } else{
                     errors++;
-                    System.out.println(errors);
-
                 }
+                System.out.println("Errors: " + errors);
             }
             
             
                
         };
         
+        /**
+         * 
+         */
         ActionListener ScreenUpdatesLister = evt -> { // Update Screen For Game start
             
         };
@@ -103,7 +124,7 @@ public class HangmanGamePanel extends JPanel{
         topPanel.setOpaque(false);
         
         skipButton = new JButton("Skip");
-        skipButton.addActionListener(skipListener);
+        skipButton.addActionListener(skipAndEndListener);
         skipButton.setActionCommand("Skip");
         skipButton.setBounds(500, 15, 70, 25);
         
@@ -149,12 +170,11 @@ public class HangmanGamePanel extends JPanel{
      */
     public void startGame(){
        resetButtons();
-       guesses = 0;
+       playerScore = 100;
        errors = 0;
        int r = (int) (Math.random() * (4)); 
        wordToFind = WORDBANK[r];                // Choose random word
        wordFound = new char[wordToFind.length()];
-       
        
        for (int i = 0; i < wordFound.length; i++) {
                wordFound[i] = '_';
