26e634bcc5565bd0c5711004d6965690c565ff57
Vivian Trieu <Vivian Trieu@10.110.205.74>
2022-10-13 01:03:01 -0700

    Add timer and absolute positioning for SudokuPanel

--- PointAndClick/src/pointandclick/Sudoku/SudokuPanel.java
+++ PointAndClick/src/pointandclick/Sudoku/SudokuPanel.java
@@ -2,6 +2,8 @@ package pointandclick.Sudoku;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import javax.swing.*;
 
 import pointandclick.Common.RoundedBorder;
@@ -11,15 +13,16 @@ public class SudokuPanel extends JPanel {
     private SudokuBoard sudokuBoard;
     private int sudokuScore;
     private boolean hasBeenSubmitted;
+    private JLabel timeLabel;
+    private Timer timeLabelTimer;
 
     public SudokuPanel(ActionListener listener) {
-        // Can't get absolute positioning to work D: (probably just dumb and forgetting something small)
-        setLayout(new FlowLayout());
+        setLayout(null);
         setPreferredSize(new Dimension(600, 400));
         this.sudokuScore = 0;
         this.hasBeenSubmitted = false;
         this.sudokuBoard = new SudokuBoard();
-        sudokuBoard.setPreferredSize(new Dimension(342,342));
+        sudokuBoard.setBounds(20, 10, 342, 342);
         add(sudokuBoard);
         
         ActionListener sudokuListener = evt -> {
@@ -46,6 +49,7 @@ public class SudokuPanel extends JPanel {
                             JOptionPane.showMessageDialog(this, "Answer is incorrect.", "Try Again", JOptionPane.ERROR_MESSAGE);
                         }
                     }
+                    break;
                 }
                 default -> {
                     throw new AssertionError();
@@ -73,6 +77,19 @@ public class SudokuPanel extends JPanel {
         submitButton.setFont(new Font("Marker Felt", Font.PLAIN, 15));
         submitButton.setToolTipText("Submit answer.");
         add(submitButton);
+        
+        SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss aa");
+        ActionListener timeListener = evt -> {
+            timeLabel.setText(format.format(new Date())); 
+        };
+        
+        timeLabel = new JLabel("");
+        timeLabel.setBounds(400, 16, 200, 25);
+        timeLabel.setFont(new Font("Marker Felt", Font.BOLD, 15));
+        timeListener.actionPerformed(null);
+        timeLabelTimer = new Timer(1000, timeListener);
+        timeLabelTimer.start();
+        add(timeLabel);
     }
     
     public int getGameScore(){
