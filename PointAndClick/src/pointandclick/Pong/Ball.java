c0ce9f3fc464a05ff8aa9a03f45fc26dc9abcefd
jasroop-singh <113493289+jasroop-singh@users.noreply.github.com>
2022-10-20 14:24:32 -0700

    Update Ball.java

--- PointAndClick/src/pointandclick/Pong/Ball.java
+++ PointAndClick/src/pointandclick/Pong/Ball.java
@@ -4,9 +4,8 @@
  */
 package pointandclick.Pong;
 
-import java.awt.Color;
-import java.awt.Rectangle;
-
+import java.awt.*;
+import java.util.Random;
 /**
  *
  * @author leothecrz
@@ -16,8 +15,10 @@ public final class Ball extends Rectangle{
     private static int BALL_WIDTH = 15;
     private static int BALL_HEIGHT = BALL_WIDTH;
     
+    
     private Color fillColor = Color.WHITE;
     
+    Random rand;
     int x;
     int y;
     double xVelocity;
@@ -28,10 +29,43 @@ public final class Ball extends Rectangle{
         this.x = x;
         this.y = y;
         setLocation(x, y);
+   
+        rand = new Random();
+        
+        //gets either 0 or 1
+        int randXDir = rand.nextInt(2);
+        int randYDir = rand.nextInt(2);
+        
+        // ball move left when 0 is chosen randomly
+        if(randXDir == 0)
+            randXDir--;
+        setXDir(randXDir);
+        
+        // ball move down when 0 is chosen randomly
+        if(randYDir == 0)
+            randYDir--;
+        setYDir(randYDir);
+       
     }
     
-    public Color getFillColor(){
-        return this.fillColor;
+    //set x direction
+    public void setXDir(int xDir) {
+        xVelocity = xDir; 
+        
+        // not fully completed yet
+    }
+    
+    //set y direction
+    public void setYDir(int yDir) {
+        yVelocity = yDir;
+        
+        // not fully completed yet
+    }
+    
+    //draw ball
+    public void draw(Graphics g) {
+        g.setColor(fillColor);
+        g.fillOval(x, y, BALL_WIDTH, BALL_HEIGHT);
     }
     
 }
